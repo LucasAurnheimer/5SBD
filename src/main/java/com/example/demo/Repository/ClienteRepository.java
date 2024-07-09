1cfcbd3e2390dcb5a9305a749726ab0488534190
Lucas <lucas.santos@provider-it.com.br>
2024-07-09 16:52:58 -0300

    removendo arquivos repetidos

--- src/main/java/com/example/demo/Repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
--- src/main/java/com/example/demo/Repository/MovimentacaoEstoqueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Model.MovimentacaoEstoque;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovimentacaoEstoqueRepository extends JpaRepository<MovimentacaoEstoque, Long> {
-}
